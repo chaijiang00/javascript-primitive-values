9a4166b5705f28574cee497541bf702e811b42a7
chaijiang <chaijiangchaisan@gmail.com>
2025-04-11 20:37:05 +0700

    edit:exercise 1-8

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,11 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000;
+let productDis = 25;
+let discount = `${(totalPrice*productDis)/100}`;
+let netPrice = totalPrice-discount;
+let discountMessage="Discount = ";
+let netPriceMessage="Net Price = ";
+console.log(discountMessage+discount);
+console.log(netPriceMessage+netPrice);
